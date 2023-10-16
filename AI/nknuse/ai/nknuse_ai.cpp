b3a2c484dda3f75b91a10d4bf9671e4bc6d2ebb4
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2023-10-16 14:59:06 +0800

    .

--- AI/nknuse/ai/nknuse_ai.cpp
+++
@@ -1,6 +0,0 @@
-#include "nknuse_ai.hpp"
-namespace nknuse {
-	namespace common {
-		std::random_device Numeric::RAND_DEVICE{};
-	}
-}
--- AI/nknuse/ai/nknuse_ai.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "component/neuron.hpp"
-#include "component/neuron_pool.hpp"
